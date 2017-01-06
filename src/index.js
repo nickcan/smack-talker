f0ac82fd8794ba3a1fdbca45f43800a6259a6037
Nicholas Cantelmi <nickcantelmi@gmail.com>
2017-01-05 18:16:31 -0800

    Params for name and about intents

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-'use strict';
-var Alexa = require('alexa-sdk');
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+"use strict";
+var Alexa = require("alexa-sdk");
+var APP_ID = undefined;
 
 var languageStrings = {
     "en-US": {
         "translation": {
-            "INSULTS": [
-                "You're not gonna make this shot! You Jackass!",
-                "Your six cup game is week.",
-                "You couldn't hit the back side of a barn with a ping pong ball.",
-                "You think you're good, you ain't. So go fuuck yourself",
-                "Sweet baby Jesus, your beer pong game is so fuuucking bad",
-                "You gonna win? Nope. Gonna sit under the table? Yep!",
-                "You may not be the worst in the house, but you aren't the best. Nick is."
-            ],
+            "INSULTS": {
+                default: [
+                    "Two wrongs don't make a right, take your parents as an example.",
+                    "I'd like to see things from your point of view but I can't seem to get my head that far up my ass.",
+                    "It's better to let someone think you are an Idiot than to open your mouth and prove it.",
+                    "You must have been born on a highway because that's where most accidents happen.",
+                    "If laughter is the best medicine, your face must be curing the world.",
+                    "I wasn't born with enough middle fingers to let you know how I feel about you."
+                ],
+                showtime: [
+                    "your showtime game is weak bitch",
+                    "I'm surprised you ever make free throws with a green zone that small",
+                    "You're not gonna make this shot! You Jackass!",
+                    "You think you're good? You're not. So go fuck yourself",
+                    "How does the ground feel? Fuck head"
+                ]
+            },
             "SKILL_NAME" : "American Insults",
             "GET_INSULT_MESSAGE" : "Here's your Insult: ",
             "HELP_MESSAGE" : "You can say talk shit, or, you can say exit... What can I help you with?",
@@ -33,14 +41,14 @@ exports.handler = function(event, context, callback) {
 };
 
 var handlers = {
-    'LaunchRequest': function() {
-        this.emit('TalkShit');
+    "LaunchRequest": function() {
+        this.emit("TalkShit");
     },
-    'GetNewInsultIntent': function() {
-        this.emit('TalkShit');
+    "GetNewInsultIntent": function() {
+        this.emit("TalkShit");
     },
-    'GetNewInsultWithNameIntent': function() {
-        var insultsArr = this.t('INSULTS');
+    "GetNewInsultWithNameIntent": function() {
+        var insultsArr = this.t("INSULTS").default;
         var insultIndex = Math.floor(Math.random() * insultsArr.length);
         var randomInsult = insultsArr[insultIndex];
 
@@ -48,27 +56,49 @@ var handlers = {
 
         var speechOutput = "Hey " + name + ", " + randomInsult;
 
-        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomInsult)
+        this.emit(":tellWithCard", speechOutput, this.t("SKILL_NAME"), randomInsult)
     },
-    'TalkShit': function() {
-        // Get a random space Insult from the space Insults list
-        // Use this.t() to get corresponding language data
-        var insultsArr = this.t('INSULTS');
+    "GetNewInsultWithNameAndCategoryIntent": function() {
+        var category = this.event.request.intent.slots.Category.value;
+
+        var insultsArr = this.t("INSULTS")[category];
         var insultIndex = Math.floor(Math.random() * insultsArr.length);
         var randomInsult = insultsArr[insultIndex];
 
-        // Create speech output
-        this.emit(':tellWithCard', randomInsult, this.t("SKILL_NAME"), randomInsult)
+        var name = this.event.request.intent.slots.Name.value;
+
+        var speechOutput = "Hey " + name + ", " + randomInsult;
+
+        this.emit(":tellWithCard", speechOutput, this.t("SKILL_NAME"), randomInsult)
     },
-    'AMAZON.HelpIntent': function() {
+    "GetNewInsultWithCategoryIntent": function() {
+        var category = this.event.request.intent.slots.Category.value;
+
+        var insultsArr = this.t("INSULTS")[category];
+        var insultIndex = Math.floor(Math.random() * insultsArr.length);
+        var randomInsult = insultsArr[insultIndex];
+
+        var speechOutput = randomInsult;
+
+        this.emit(":tellWithCard", speechOutput, this.t("SKILL_NAME"), randomInsult)
+    },
+    "TalkShit": function() {
+        var insultsArr = this.t("INSULTS").default;
+        var insultIndex = Math.floor(Math.random() * insultsArr.length);
+        var randomInsult = insultsArr[insultIndex];
+
+        this.emit(":tellWithCard", randomInsult, this.t("SKILL_NAME"), randomInsult)
+    },
+    "AMAZON.HelpIntent": function() {
         var speechOutput = this.t("HELP_MESSAGE");
         var reprompt = this.t("HELP_MESSAGE");
-        this.emit(':ask', speechOutput, reprompt);
+
+        this.emit(":ask", speechOutput, reprompt);
     },
-    'AMAZON.CancelIntent': function() {
-        this.emit(':tell', this.t("STOP_MESSAGE"));
+    "AMAZON.CancelIntent": function() {
+        this.emit(":tell", this.t("STOP_MESSAGE"));
     },
-    'AMAZON.StopIntent': function() {
-        this.emit(':tell', this.t("STOP_MESSAGE"));
+    "AMAZON.StopIntent": function() {
+        this.emit(":tell", this.t("STOP_MESSAGE"));
     }
 };
